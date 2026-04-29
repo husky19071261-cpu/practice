b48e3b977bcea8c606d17b7dbff43c89267ed2dd
husky19071261-cpu <husky19071261@gmail.com>
2026-04-28 18:30:10 -0600

    Add animation to box movement in script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-document.getElementById("btn").addEventListener("click", () => {
-  alert("You just ran JavaScript");
-});
+let x = 100;
+let y = 100;
+let vx = 2;
+let vy = 1.5;
+
+const box = document.getElementById("box");
+
+function animate() {
+  x += vx;
+  y += vy;
+
+  box.style.transform = `translate(${x}px, ${y}px)`;
+
+  requestAnimationFrame(animate);
+}
+
+animate();
